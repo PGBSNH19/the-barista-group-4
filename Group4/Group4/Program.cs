1b940196b50bb1d398ce1119cdaba419dcc22d3a
zorkii <micael.wollter@gmail.com>
2020-03-05 15:55:04 +0100

    Driver @aohzork

--- Group4/Group4/Program.cs
+++ Group4/Group4/Program.cs
@@ -6,31 +6,56 @@ using System.Threading.Tasks;
 
 namespace Group4
 {
+    public enum Coffee
+    {
+        Americano,
+        Espresso,
+        Cappuccino,
+        Latte,
+        Mocha,
+        Macchiato
+    }
+
+    public enum Beans
+    {
+        Arabica,
+        Robusta,
+        Liberia
+    }
+
+    public interface IName
+    {
+        IMakeBeverage CoffeeName(Coffee coffee);
+    }
+
+    public interface IMakeBeverage
+    {
+        IMakeBeverage AddBeans(Beans beans);
+        IMakeBeverage GrindBeans(bool grounded);
+        IMakeBeverage AddWater(int ml);
+        IMakeBeverage AddSteamedMilk(int ml);
+        IMakeBeverage AddMilkFoam(int ml);
+        IMakeBeverage AddChocolateSyrup(int ml);
+        IMakeBeverage AddWhippedCream(int ml);
+        string Serve();
+    }
+
+    class FluentCoffee : IMakeBeverage, IName
+    {
+    public string Name { get; set; }
+    public string BeanType { get; set; }
+    public bool IsGrounded { get; set; }
+    public int Water { get; set; }
+    public int SteamedMilk { get; set; }
+    public int MilkFoam { get; set; }
+    public int ChocolateSyrup { get; set; }
+    public int WhippedCream { get; set; }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please choose type of coffe!");
-            Console.WriteLine("1 Espresso");
-            Console.WriteLine("2 Latte");
-            Console.WriteLine("3 Black coffe");
-            int choice = int.Parse(Console.ReadLine());
-
-            switch (choice)
-            {
-                case 1:                   
-                        break;
-                case 2:
-                    break;
-
-                case 3:
-                    break;
-            }
-
-
-            var name = new CoffeeMachine().WaterVolume()
-                .AddBeans();
-
+            
 
         }
     }
