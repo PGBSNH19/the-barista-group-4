2f641370b6bf190b073ea990923e83e71af44b66
zorkii <micael.wollter@gmail.com>
2020-03-05 15:56:41 +0100

    Driver @samirehsani19

--- Group4/Group4/Program.cs
+++ Group4/Group4/Program.cs
@@ -42,21 +42,77 @@ namespace Group4
 
     class FluentCoffee : IMakeBeverage, IName
     {
-    public string Name { get; set; }
-    public string BeanType { get; set; }
-    public bool IsGrounded { get; set; }
-    public int Water { get; set; }
-    public int SteamedMilk { get; set; }
-    public int MilkFoam { get; set; }
-    public int ChocolateSyrup { get; set; }
-    public int WhippedCream { get; set; }
-    
+        public string Name { get; set; }
+        public string BeanType { get; set; }
+        public bool IsGrounded { get; set; }
+        public int Water { get; set; }
+        public int SteamedMilk { get; set; }
+        public int MilkFoam { get; set; }
+        public int ChocolateSyrup { get; set; }
+        public int WhippedCream { get; set; }
+
+        public IMakeBeverage CoffeeName(Coffee coffee)
+        {
+            Name = coffee.ToString();
+            return this;
+        }
+
+        public IMakeBeverage AddBeans(Beans beans)
+        {
+            BeanType = beans.ToString();
+            return this;
+        }
+        public IMakeBeverage GrindBeans(bool grounded)
+        {
+            IsGrounded = grounded;
+            return this;
+        }
+        public IMakeBeverage AddWater(int ml)
+        {
+            Water += ml;
+            return this;
+        }
+        public IMakeBeverage AddSteamedMilk(int ml)
+        {
+            SteamedMilk = ml;
+            return this;
+        }
+        public IMakeBeverage AddMilkFoam(int ml)
+        {
+            MilkFoam = ml;
+            return this;
+        }
+        public IMakeBeverage AddChocolateSyrup(int ml)
+        {
+            ChocolateSyrup = ml;
+            return this;
+        }
+        public IMakeBeverage AddWhippedCream(int ml)
+        {
+            WhippedCream = ml;
+            return this;
+        }
+
+        public string Serve()
+        {
+            return this.BeanType;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var espresso = new FluentCoffee().CoffeeName(Coffee.Espresso).AddBeans(Beans.Liberia).GrindBeans(true).AddWater(30).Serve();
+            Console.WriteLine(espresso);
 
+            var mocha = new FluentCoffee().CoffeeName(Coffee.Mocha).AddBeans(Beans.Robusta).GrindBeans(true).AddWater(30).AddChocolateSyrup(20).AddSteamedMilk(25).AddWhippedCream(20).Serve();
+            Console.WriteLine(mocha);
+
+            var americano = new FluentCoffee().CoffeeName(Coffee.Americano).AddBeans(Beans.Arabica).GrindBeans(true).AddWater(30).AddWater(60).Serve();
+            Console.WriteLine(americano);
+
+            Console.ReadKey();
         }
     }
 }
